206d3dc7823e6d2b1eeb9a0c527aee816f5513c7
cmurugan <cmurugan@iotserver.mcci.com>
2018-02-20 11:32:50 +0530

    amazon s3 bucket changes

--- influxdb-backup/backup.sh
+++ influxdb-backup/backup.sh
@@ -12,3 +12,13 @@ for db in ${DATABASES//:/ }; do
   influxd backup -database $db -host $INFLUX_HOST:8088 /var/lib/influxdb-backup
 done
 
+if [ $? -eq 0 ]; then
+
+        tar czf /var/lib/amazon-bucket/metdata_db_backup_`date +%F`.tar.gz /var/lib/influxdb-backup/
+        tar czf /var/lib/amazon-bucket/data_directory_backup_`date +%F`.tar.gz /var/lib/influxdb/
+        aws s3  sync /var/lib/amazon-bucket/ s3://${S3_BUCKET_INFLUXDB}/
+
+fi
+
+find /var/lib/amazon-bucket/ -type f -mtime +90 -exec rm {} \;
+

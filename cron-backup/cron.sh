f098557e8cef8790e29de05abec2d1ace61c90e1
MuruganChandrasekar <cmurugan@mcci.com>
2021-03-25 10:07:58 +0000

    Adding backup settings

--- cron-backup/cron.sh
+++ cron-backup/cron.sh
@@ -10,7 +10,9 @@ cd /root
 crontab -l > mycron || echo "no crontab for root, going on"
 
 #echo new cron into cron file
-echo "35 6 * * * /bin/bash -l -c '/bin/mongodb_backup.sh'" >> mycron
+echo "35 6 * * * /bin/bash -l -c '/bin/nodered_backup.sh'" >> mycron
+echo "35 7 * * * /bin/bash -l -c '/bin/grafana_backup.sh'" >> mycron
+echo "35 8 * * * /bin/bash -l -c '/bin/nginx_backup.sh'" >> mycron
 
 #delete duplicated lines
 sort -u -o mycron mycron

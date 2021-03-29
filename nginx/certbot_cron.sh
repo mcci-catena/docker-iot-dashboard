6403b3385b21da29b389ff3a5cd1ef90770b68f0
Murugan Chandrasekar <cmurugan@mcci.com>
2021-03-29 12:24:12 +0530

    Update certbot_cron.sh
    Crontab fine tuning

--- nginx/certbot_cron.sh
+++ nginx/certbot_cron.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+# exit on unchecked errors
+set -e
+
+# backups are scheduled via the root crontab. Start by heading there
 cd /root
+
 #write out current crontab
-crontab -l > mycron
+crontab -l > mycron || echo "no crontab for root, going on"
 
 #echo new cron into cron file
 echo "15 3 * * * /usr/bin/certbot renew --preferred-challenges http-01,dns-01" >> mycron
+
+#delete duplicated lines
+sort -u -o mycron mycron
 
 #install new cron file
 crontab mycron
